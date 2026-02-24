d40ea85e1a2ab9158c2739a18cedf4ac95feefcf
amirylegend-ctrl <amirylegend@gmail.com>
2026-02-25 00:14:22 +0330

    SinglCMD-NoName14213987((M)).py

--- SinglCMD-NoName14213987((M)).py
+++ SinglCMD-NoName14213987((M)).py
@@ -2247,7 +2247,7 @@ class SinglCMD(Plugin):
             return  
         
         z = GCM()
-        teck(0.03, CallStrict(s.ear))
+        teck(0.1, CallStrict(s.ear))
         if z == s.z: return
         s.z = z
         
